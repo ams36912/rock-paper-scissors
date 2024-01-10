c4acff207607d2d2cb78dffc1ced689980e86058
Amy Street <amymorganstreet@gmail.com>
2024-01-09 20:38:08 -0500

    Fix errors in game functionality

--- myscripts.js
+++ myscripts.js
@@ -1,105 +1,74 @@
 // create computer choice fuction that will return rock, paper, or scissors
-function getComputerChoice() {
-    let computerChoice = Math.random();
-    //console.log(computerChoice);
-    let computerPlay = (computerChoice < 0.33) ? 'Rock' :
-        (computerChoice < 0.66) ? 'Paper' :
-        'Scissors';
-        //console.log(computerPlay);
-    return computerPlay;
+function computerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+        case 0:
+            return 'Rock'
+        case 1:
+            return 'Paper'
+        case 2:
+            return 'Scissors'
+  }
 }
-
-
 
 // get player choice
-function getPlayerChoice() {
-    let playerChoice = prompt("Choose rock, paper, or scissors.").toLowerCase();
+function playerChoice() {
+    let choice = prompt("Choose rock, paper, or scissors.");
+    choice = choice.substring(0,1).toUpperCase() + choice.substring(1).toLowerCase();
     //console.log(playerChoice);
-    let playerPlay = ( playerChoice === 'rock') ? 'Rock' :
-        ( playerChoice === 'paper') ? 'Paper' :
-        ( playerChoice === 'scissors') ? 'Scissors' :
-        'Invalid weapon';
-        //console.log(playerPlay);
-    return playerPlay;
+    if (!(choice === 'Rock' || choice === 'Paper' || choice === 'Scissors')){
+        choice = '';
+        //console.log("INVALID");
+    }
+    return choice;
 }
+
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = '';
+
 
 // create function where one round of rock paper scissors is played
 function playRound( playerSelection, computerSelection ) {
-    console.log('Player Selection: ' + playerSelection + ' Computer Selection: ' + computerSelection);
-    //Declare victory/loss statement
-    let roundResult = "";
-    if (playerSelection === computerSelection){
-        //it's a tie
-        roundResult = "tie";
-            //console.log(`It is a tie. Play again.`);
-            //playRound(getComputerChoice(), getPlayerChoice());
-        //return;
-    } else if (playerSelection === 'Rock'){
-        if (computerSelection === 'Paper'){
-            // player loses
-            roundResult = "loss";
-        } else /* computerSelection is Scissors */{
-            // player wins
-            roundResult = "win";
-        }
-    } else if (playerSelection === 'Paper') {
-        if ( computerSelection === 'Rock'){
-            // player wins
-            roundResult = "win";
-        } else /* computerSelection is Scissors */ {
-            // player loses
-            roundResult = "loss"
-        }
-    } else /* playerSelection is Scissors */ {
-        if (computerSelection === 'Rock'){
-            // player loses
-            roundResult = "loss";
-        } else /* computerSelection is Paper */ {
-            // player wins
-            roundResult = "win";
-        }
+    
+    if(!(playerSelection === 'Rock' || playerSelection === 'Paper' || playerSelection === 'Scissors')){
+        console.log(`That is an invalid weapon.`);
+        return;
     }
-
-    // should never run
-    if (roundResult === undefined) {
-        playRound(getComputerChoice(), getPlayerChoice());
-        console.log("round result is undefined");
+    if (playerSelection === computerSelection ){
+        roundWinner = 'tie';
+        console.log(`It was a tie, try again. ${playerSelection} beats ${computerSelection}`);
+        return;
     }
-    // ^^ should never run
-
-    if (roundResult === 'tie') {
-        console.log(`It is a tie. Play again.`);
-        playRound(getComputerChoice(), getPlayerChoice());
-    } else if (roundResult === 'win') {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-    } else if (roundResult === 'loss') {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper')){
+            playerScore++;
+            roundWinner = 'Player';
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            
     }
+    if ((computerSelection === 'Rock' && playerSelection === 'Scissors') ||
+        (computerSelection === 'Paper' && playerSelection === 'Rock') ||
+        (computerSelection === 'Scissors' && playerSelection === 'Paper')){
+            computerScore++;
+            roundWinner = 'Computer';
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            
+    }
+    console.log(`The score is Player: ${playerScore} Computer: ${computerScore}`);
+    console.log('***');   
+    return;
 
-    return roundResult;
 }
 
-//play a best-of-five game
+// //play a best-of-five game
 function game(){
-    let playerScore = 0;
-    let computerScore = 0;
 
-    playRound(getComputerChoice(), getPlayerChoice());
-
-     for (let i = 0; i < 5; ) {
-        let result = playRound(getComputerChoice(), getPlayerChoice());
-        //console.log("RESULT " + result);
-        
-        if(result === 'win'){
-            playerScore++;
-            i++;
-        } else if(result === 'loss'){
-            computerScore++;
-            i++;
-        } else {}
-        console.log(`The score is Player: ${playerScore} Computer: ${computerScore}.`);
-     }
-    
+    while((playerScore + computerScore) < 5){
+        playRound(playerChoice(),computerChoice());
+    }
+    console.log(`The game is finished.`);  
 }
 
 game();
